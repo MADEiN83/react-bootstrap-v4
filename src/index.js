597b498488b5dfd7606800c8ab51ba5a7ad8f5a9
MADEiN83 <Anthony.Distefano@hotmail.fr>
2018-04-09 21:34:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,57 @@
 import Alert from './components/Alert';
 import Badge from './components/Badge';
 
-export { Alert, Badge };
+import Button from './components/Button';
+import Breadcrumb from './components/Breadcrumb';
+import Card from './components/Card';
+import DismissButton from './components/DismissButton';
+import ListGroup from './components/ListGroup';
+import ListGroupItem from './components/ListGroupItem';
+import Progress from './components/Progress';
 
-// export Button from './components/Button';
-// export Breadcrumb from './components/Breadcrumb';
-// export Card from './components/Card';
-// export ListGroup from './components/ListGroup';
-// export Progress from './components/Progress';
+import Row from './components/Row';
+import Column from './components/Column';
 
-// export Row from './components/Row';
-// export Column from './components/Column';
+import Table from './components/Table';
+import TableRow from './components/TableRow';
+import TableCell from './components/TableCell';
 
-// export Table from './components/Table';
-// export TableRow from './components/TableRow';
-// export TableCell from './components/TableCell';
+import Pagination from './components/Pagination';
 
-// export Pagination from './components/Pagination';
+import Form from './components/Form';
+import Input from './components/Input';
+import InputEmail from './components/InputEmail';
+import InputPassword from './components/InputPassword';
+import InputCheckbox from './components/InputCheckbox';
+import InputText from './components/InputText';
+import TextArea from './components/TextArea';
+import Select from './components/Select';
+import InputRadio from './components/InputRadio';
 
-// export Form from './components/Form';
-// export Input from './components/Input';
-// export InputPassword from './components/InputPassword';
-// export InputCheckbox from './components/InputCheckbox';
-// export InputText from './components/InputText';
-// export TextArea from './components/TextArea';
-// export Select from './components/Select';
-// export InputRadio from './components/InputRadio';
+import BootstrapV4 from './components/BootstrapV4';
 
-// export BootstrapV4 from './components/BootstrapV4';
+export { 
+    Alert,
+    Badge,
+    Button,
+    Card,
+    Column,
+    DismissButton,
+    Form,
+    Input,
+    InputCheckbox,
+    InputEmail,
+    InputPassword,
+    InputRadio,
+    InputText,
+    ListGroup,
+    ListGroupItem,
+    Pagination,
+    Progress,
+    Row,
+    Select,
+    Table,
+    TableCell,
+    TableRow,
+    TextArea,
+};
